af338b479f70342202f28d3b0ab7c0348d11e31a
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2020-11-02 18:11:53 -0300

    :sparkles: Created multiple inheritance and using mixin
    close #31

--- object-oriented_programming/dogs/dog.py
+++ object-oriented_programming/dogs/dog.py
@@ -26,3 +26,51 @@ class Dog(Mammal):
 
     def __eq__(self, other):
         return isinstance(other, Dog) and self.__dict__ == other.__dict__
+
+
+class BigMixin:
+    """
+    Mixin: change bark
+    """
+    def bark(self, qty=1):
+        qty += self.angry * qty
+        print(f"{self.name}:{' Wuff!' * qty}")
+
+
+class Mastiff(BigMixin, Dog):
+    """
+    Mastiff barks diferent
+
+    >>> atos = Mastiff('Atos')
+    >>> atos.bark()
+    Atos: Wuff!
+    """
+
+
+class StBernard(BigMixin, Dog):
+    """
+    St. Bernard serve brandy
+
+    >>> sansao = StBernard('Sansao')
+    >>> sansao.bark()
+    Sansao: Wuff!
+    >>> sansao.serve()
+    Sansao serves brandy (9 doses left)
+    >>> sansao.doses = 1
+    >>> sansao.serve()
+    Sansao serves brandy (0 doses left)
+    >>> sansao.serve()
+    Traceback (most recent call last):
+      ...
+    ValueError: Brandy ends!
+    """
+    def __init__(self, name):
+        super().__init__(name)
+        self.doses = 10
+
+    def serve(self):
+        if self.doses == 0:
+            raise ValueError("Brandy ends!")
+        self.doses -= 1
+        msg = f"{self.name} serves brandy ({self.doses} doses left)"
+        print(msg)
